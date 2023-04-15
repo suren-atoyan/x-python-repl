a9dd00329c2a2d5459f4732b4320ab7cc8be3be2
suren-atoyan <contact@surenatoyan.com>
2023-04-15 18:46:37 +0400

    remove theme provider

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil';
-import { ThemeProvider } from '@emotion/react';
 
 import REPL from '../../dist';
 
 function App() {
   return (
     <RecoilRoot>
-      <ThemeProvider theme={{}}>
-        <div
-          style={{
-            height: '100vh',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: '100%',
-          }}
-        >
-          <REPL />
-        </div>
-      </ThemeProvider>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+        }}
+      >
+        <REPL />
+      </div>
     </RecoilRoot>
   );
 }
